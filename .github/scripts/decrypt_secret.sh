e2eac46f81378aa0318683aa1d376a40d6b85011
Dan Gilbert <me@dan-gilbert.uk>
2020-01-19 18:48:33 +0100

    Include key properties for release

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,3 +5,5 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_DEPLOY_KEY_SECRET_PASSPHRASE" \
 --output $HOME/secrets/key.jks android/app/key.jks.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$KEY_PROPERTIES_SECRET_PASSPHRASE" \
+--output $HOME/secrets/key.properties android/key.properties.gpg
