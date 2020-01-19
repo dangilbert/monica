b8a5b0fc7cf6a5790bdd27652baa7e6e12ca5c06
Dan Gilbert <me@dan-gilbert.uk>
2020-01-19 19:35:04 +0100

    Actually add the files this time

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -7,3 +7,5 @@ gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_DEPLOY_KEY_SECRET_PAS
 --output $HOME/secrets/key.jks android/app/key.jks.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$KEY_PROPERTIES_SECRET_PASSPHRASE" \
 --output $HOME/secrets/key.properties android/key.properties.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_PLAY_SERVICE_ACCOUNT_SECRET_PASSPHRASE" \
+--output $HOME/secrets/google-play-service.json android/google-play-service.json.gpg
